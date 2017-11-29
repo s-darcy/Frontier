e6d5c7cbce01a39cd21f13a392795c03b7d6a0fe
Shane <sdarcy@redventures.com>
2017-11-29 17:02:28 -0500

    worked on CSS

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,20 +1,24 @@
 // Initialize Foundation
 // jQuery( document ).ready( function( $ ) {
 // 	$( document ).foundation();
-$(document).ready(function(){
 
-    if($(window).width() < 480){
-        $( '#not-mobile' ).html('<div class="not-mobile--move-to-top">'
-                + '<div class="columns large-5">'
-                    + '<span>Call to order now!</span>'
-                    + '<a href=""><img alt="telephone icon" src="assets/img/icons/tel.svg" />1.888.888.8888</a>'
-                    + '<div class="mobile">'
-                        + '<img alt="hamburger navigation icon" src="assets/img/icons/navicon.svg" />'
-                    + '</div>'
-                + '</div>'
-            +'</div>'
-        );
-    }   
-});     
+
+
+//injecting HTML with jQuery
+// $(document).ready(function(){
+
+//     if($(window).width() < 480){
+//         $( '#not-mobile' ).html('<div class="not-mobile--move-to-top">'
+//                 + '<div class="columns large-5">'
+//                     + '<span>Call to order now!</span>'
+//                     + '<a href=""><img alt="telephone icon" src="assets/img/icons/tel.svg" />1.888.888.8888</a>'
+//                     + '<div class="mobile">'
+//                         + '<img alt="hamburger navigation icon" src="assets/img/icons/navicon.svg" />'
+//                     + '</div>'
+//                 + '</div>'
+//             +'</div>'
+//         );
+//     }   
+// });     
 
 
